31179f69cc6a71d9f56816209de541aaac696fe2
Chris Jackson <hello@cjjackson.dev>
2025-08-22 13:53:08 +0100

    get rid of last modified

--- run_release.py
+++ run_release.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-import datetime
 import uuid
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 etag = str(uuid.uuid4())
-
-http_last_modified = datetime.datetime.now(datetime.UTC).strftime("%a, %d %b %Y %H:%M:%S GMT")
 
 cmds: list = [
     ["./run_tailwind.py"],
